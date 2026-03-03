1c61cd1756b468cf3cc8246d037373c79a5cf6b2
miguel <mallquilm@gmail.com>
2026-03-02 20:20:30 -0500

    cap2-lab06

--- src/main/java/edu/cibertec/spring_mvc_42v/resources/controller/UsuarioController.java
+++ src/main/java/edu/cibertec/spring_mvc_42v/resources/controller/UsuarioController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
